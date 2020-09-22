b2fc2d5210a56129e13537680de5a65afabb2263
Nate <nathanieldiamond97@gmail.com>
2020-09-22 13:06:49 -0600

    the filter search works when i hit enter but not click the button. also it does not populate the table rather console log works so that a start

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -15,13 +15,29 @@ var shape = body.append('td');
 var duration = body.append('td');
 var comments = body.append('td');
 
-for(let i = 0; i < tableData.length; i++){
-    cell.text(tableData[i].datetime);
-    city.text(tableData[i].city);
-    state.text(tableData[i].state);
-    country.text(tableData[i].country);
-    shape.text(tableData[i].shape)
-    duration.text(tableData[i].durationMinutes);
-    comments.text.text(tableData[i].comments)
 
-}
+
+var form = d3.select('form');
+form.on('submit', runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+    var inputElement = d3.select('input');
+    var inputValue = inputElement.property('value');
+    var filtered = tableData.filter(tableData => tableData.datetime === inputValue);
+    console.log(filtered);
+    for(let i = 0; i < tableData.length; i++){
+        cell.text(tableData[i].datetime);
+        city.text(tableData[i].city);
+        state.text(tableData[i].state);
+        country.text(tableData[i].country);
+        shape.text(tableData[i].shape);
+        duration.text(tableData[i].durationMinutes);
+        comments.text(tableData[i].comments);
+    
+    };
+    
+
+};
+
+
