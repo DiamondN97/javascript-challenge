1ea055b37b39c5c7c25ed703dfa8c94585ae3d8a
Nate <nathanieldiamond97@gmail.com>
2020-09-23 09:09:02 -0600

    the table is populating with data when i search... but it's for the wrong days

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,14 +7,6 @@ var table = d3.select('table');
 var body = d3.select('body');
 var tbody=d3.select('tbody')
 
-var row = tbody.append('tr');
-var cell = tbody.append('td');
-var city = tbody.append('td');
-var state = tbody.append('td');
-var country = tbody.append('td');
-var shape = tbody.append('td');
-var duration = tbody.append('td');
-var comments = tbody.append('td');
 
 
 
@@ -26,19 +18,31 @@ function runEnter(){
     var inputElement = d3.select('input');
     var inputValue = inputElement.property('value');
     var filtered = tableData.filter(tableData => tableData.datetime === inputValue);
-    console.log(filtered);
-    for(let i = 0; i < tableData.length; i++){
-        cell.text(tableData[i].datetime);
-        city.text(tableData[i].city);
-        state.text(tableData[i].state);
-        country.text(tableData[i].country);
-        shape.text(tableData[i].shape);
-        duration.text(tableData[i].durationMinutes);
-        comments.text(tableData[i].comments);
-        tbody.html()
-    };
+    // console.log(filtered);
+    var table = d3.select('table');
+    tbody.html('');
     
+for(let i = 0; i < filtered.length; i++){
+    var row = tbody.append('tr');
+    var cell = tbody.append('td');
+    var city = tbody.append('td');
+    var state = tbody.append('td');
+    var country = tbody.append('td');
+    var shape = tbody.append('td');
+    var duration = tbody.append('td');
+    var comments = tbody.append('td')
+    cell.text(tableData[i].datetime);
+    city.text(tableData[i].city);
+    state.text(tableData[i].state);
+    country.text(tableData[i].country);
+    shape.text(tableData[i].shape);
+    duration.text(tableData[i].durationMinutes);
+    comments.text(tableData[i].comments);
 
-};
+
+    };
+}
+
+
 
 
