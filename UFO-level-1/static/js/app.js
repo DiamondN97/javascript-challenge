1598f18eeb81163802b0acdda8ccb7b0daf92eff
Nate <nathanieldiamond97@gmail.com>
2020-09-24 08:51:06 -0600

    search query works now, but the page auto loads with filled table before anything is searched

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,9 +9,12 @@ var tbody=d3.select('tbody')
 
 
 
+tableInfo(tableData);
 
 var form = d3.select('form');
 form.on('submit', runEnter);
+
+
 
 function runEnter(){
     d3.event.preventDefault();
@@ -19,30 +22,27 @@ function runEnter(){
     var inputValue = inputElement.property('value');
     var filtered = tableData.filter(tableData => tableData.datetime === inputValue);
     // console.log(filtered);
-    var table = d3.select('table');
     tbody.html('');
-    
-for(let i = 0; i < filtered.length; i++){
-    var row = tbody.append('tr');
-    var cell = tbody.append('td');
-    var city = tbody.append('td');
-    var state = tbody.append('td');
-    var country = tbody.append('td');
-    var shape = tbody.append('td');
-    var duration = tbody.append('td');
-    var comments = tbody.append('td')
-    cell.text(tableData[i].datetime);
-    city.text(tableData[i].city);
-    state.text(tableData[i].state);
-    country.text(tableData[i].country);
-    shape.text(tableData[i].shape);
-    duration.text(tableData[i].durationMinutes);
-    comments.text(tableData[i].comments);
+    tableInfo(filtered);
+};
 
-
+function tableInfo(sightingInfo){
+    for(let i = 0; i < sightingInfo.length; i++){
+        var row = tbody.append('tr');
+        var cell = tbody.append('td');
+        var city = tbody.append('td');
+        var state = tbody.append('td');
+        var country = tbody.append('td');
+        var shape = tbody.append('td');
+        var duration = tbody.append('td');
+        var comments = tbody.append('td')
+        cell.text(sightingInfo[i].datetime);
+        city.text(sightingInfo[i].city);
+        state.text(sightingInfo[i].state);
+        country.text(sightingInfo[i].country);
+        shape.text(sightingInfo[i].shape);
+        duration.text(sightingInfo[i].durationMinutes);
+        comments.text(sightingInfo[i].comments);
     };
-}
-
-
-
+};
 
